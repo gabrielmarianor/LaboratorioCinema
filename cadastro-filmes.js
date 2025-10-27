a931d7f649b9cb1889409ca768f485c512cd90ba
Gabriel Mariano Ramos <gmarianoramos@gmail.com>
2025-10-27 00:44:27 -0300

    Adiciona versão completa do projeto com CSS e JS

--- cadastro-filmes.js
+++ cadastro-filmes.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     form.addEventListener('submit', (evento) => {
         evento.preventDefault(); 
 
+        // O código que faltava estava aqui:
         const titulo = document.getElementById('titulo').value;
         const descricao = document.getElementById('descricao').value;
         const genero = document.getElementById('genero').value;
@@ -21,9 +22,10 @@ document.addEventListener('DOMContentLoaded', () => {
             dataEstreia
         };
 
-        const filmes = JSON.parse(localStorage.getItem('filmes')) || []; [cite: 32]
+        // E aqui também:
+        const filmes = JSON.parse(localStorage.getItem('filmes')) || [];
         filmes.push(filme);
-        localStorage.setItem('filmes', JSON.stringify(filmes)); [cite: 32]
+        localStorage.setItem('filmes', JSON.stringify(filmes));
 
         alert('Filme salvo com sucesso!');
         form.reset();
